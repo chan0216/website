cab24d80c1b17c25f32dabda6c1dc249443a3006
chan0216 <94737861+chan0216@users.noreply.github.com>
2022-02-08 22:44:36 +0800

    Delete week-6 directory

--- week-6/view/error.py
+++
@@ -1,6 +0,0 @@
-from flask import request ,render_template,Blueprint
-error_blueprint = Blueprint('error', __name__)
-@error_blueprint.route("/error/")
-def error():
-    message=request.args.get("message")
-    return  render_template("error.html",string=message)
--- week-6/view/member.py
+++
@@ -1,8 +0,0 @@
-from flask import redirect,render_template,session,Blueprint
-member_blueprint = Blueprint('member', __name__)
-@member_blueprint.route("/member/")
-def member():
-    if('name' not in session):
-       return redirect("/")
-    name=session["name"]
-    return render_template("member.html",name=name)
